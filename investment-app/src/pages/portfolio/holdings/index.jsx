0d4bfbb036c69d94897e8a85619addd9ac0e3284
Edwin Lim <edwinlzs97@gmail.com>
2020-09-06 00:22:06 +0800

    Create index.jsx

---
+++ investment-app/src/pages/portfolio/holdings/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../../../components/common/Layout";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Holdings = () => {
+    return (
+        <Layout>
+          
+        </Layout>
+    );
+};
+
+export default Holdings;
