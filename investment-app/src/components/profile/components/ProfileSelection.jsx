b923eb9c01bcf3e006e09457aacffe8f312b2125
hf20ong <54702480+hf20ong@users.noreply.github.com>
2020-09-05 19:57:32 +0800

    Update ProfileSelection.jsx

--- investment-app/src/components/profile/components/ProfileSelection.jsx
+++ investment-app/src/components/profile/components/ProfileSelection.jsx
@@ -20,6 +20,9 @@ const ProfileSelection = () => {
     const [show, setShow] = React.useState(false);
     const handleClose = () => setShow(false);
 
+    const [knowledge, setKnowledge] = React.useState(false);
+    console.log(knowledge);
+
     return (
         <div>
             <Container>
@@ -30,7 +33,7 @@ const ProfileSelection = () => {
                 <CardDeck>
                     <Card style={{ width: '15rem' }}>
                         <Card.Body>
-                            <Button onClick={() => setShow(true)} block>Not at all</Button>
+                            <Button onClick={() => setShow(true) } block>Not at all</Button>
                         </Card.Body>
                     </Card>
                     <Card style={{ width: '15rem' }}>
@@ -43,7 +46,7 @@ const ProfileSelection = () => {
                             <Button onClick={() => 
                                 {message<3 ? 
                                 setOptions(message + 1) : 
-                                setShow(true)}} block>Somewhat</Button>
+                                (setShow(true), setKnowledge(true))}} block>Somewhat</Button>
                         </Card.Body>
                     </Card>
                     <Card style={{ width: '15rem' }}>
@@ -51,7 +54,7 @@ const ProfileSelection = () => {
                         <Button onClick={() => 
                             {message<3 ? 
                             setOptions(message + 1) : 
-                            setShow(true)}} block>Very</Button>
+                            (setShow(true), setKnowledge(true))}} block>Very</Button>
                         </Card.Body>
                     </Card>
                 </CardDeck>
@@ -62,10 +65,14 @@ const ProfileSelection = () => {
             <Modal.Header>
                 {message<3
                 ? <Modal.Title>No Worries!</Modal.Title>
+                : knowledge == false 
+                ? <Modal.Title>No Worries!</Modal.Title>
                 : <Modal.Title>Congrats!</Modal.Title>}
             </Modal.Header>
 
             {message<3
+                ? <Modal.Body>Learn more about investing in our Academy.</Modal.Body>
+                : knowledge == false 
                 ? <Modal.Body>Learn more about investing in our Academy.</Modal.Body>
                 : <Modal.Body>You know your stuff! Try out our investing simulation to get a feel on actual investing.</Modal.Body>}
             
@@ -74,6 +81,8 @@ const ProfileSelection = () => {
                 {/* TO CHANGE */}
 
                 {message<3
+                ? <Button href='/academy' block>Start Learning</Button>
+                : knowledge == false 
                 ? <Button href='/academy' block>Start Learning</Button>
                 : <Button href='/portfolio' block>Start Investing</Button>
             }
