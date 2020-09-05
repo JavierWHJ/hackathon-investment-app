2b95dfa3ad8b620fd59c56ddaa5f233c263d59b4
hf20ong <54702480+hf20ong@users.noreply.github.com>
2020-09-05 20:05:13 +0800

    Update ProfileHeader.jsx

--- investment-app/src/components/profile/components/ProfileHeader.jsx
+++ investment-app/src/components/profile/components/ProfileHeader.jsx
@@ -3,8 +3,8 @@ import styles from '../../../../styles/Home.module.scss'
 
 export default function ProfileHeader() {
     return (
-        <div style={{backgroundColor: 'lightgrey', height: '50px'}}>
-            <h5 className={styles.description}>Check your knowledge on investing</h5>
+        <div style={{backgroundColor: 'lightgrey', height: '40px'}}>
+            <h5 className={styles.description}>Check how much you know on investing!</h5>
         </div>
     )
 }
