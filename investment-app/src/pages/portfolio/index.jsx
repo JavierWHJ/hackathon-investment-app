e811f15bfd7ed7714634501a1cb6359c6e498799
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-09-05 19:43:58 +0800

    Add trading view api for live stock data

--- investment-app/src/pages/portfolio/index.jsx
+++ investment-app/src/pages/portfolio/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Layout from '../../components/common/Layout';
+import { TradingViewEmbed, widgetType } from "react-tradingview-embed";
 
 const Portfolio = () => {
   return (
-    <div>
-      
-    </div>
+      <Layout>
+          <div className="container">
+              <TradingViewEmbed
+                  widgetType={widgetType.ADVANCED_CHART}
+                  widgetConfig={{
+                      colorTheme: "light",
+                      symbol: "BITMEX:XBTUSD",
+                      width: "100%",
+                  }}
+              />
+          </div>
+      </Layout>
   );
 }
 
