8d04d49e960c48f208b8425c4b0591eb8abf99a7
jonathanlxh <jonathanlxh@users.noreply.github.com>
2020-09-05 13:59:10 +0800

    Update Loginmodal.jsx

--- investment-app/src/components/common/Loginmodal.jsx
+++ investment-app/src/components/common/Loginmodal.jsx
@@ -20,13 +20,11 @@ const Loginmodal = () => {
                     
                 <form>
                     <Modal.Body>   
-                        <div><input placeholder='Email Address' name='email' type='email' id='email'></input></div>
-                        <div><input placeholder='Name' name='name' type='text' id='text'></input></div>
+                        <div class='email'><input placeholder='Email Address' name='email' type='email' id='email' class='s2 form-control'></input></div>
+                        <div class='name'><input placeholder='Name' name='name' type='text' id='text' class='s2 form-control'></input></div>
+                        <Button variant="primary" class='btn btn-primary btn-block'>Login</Button>
                     </Modal.Body>
-
-                    <Modal.Footer>
-                        <Button variant="primary">Login</Button>
-                    </Modal.Footer>
+                        <p class='text-center'>Don't have an account? Sign up</p>
                 </form>
             </Modal>
         </div>
