9066709b98c3e5076531228e2bf223a69aa24b9b
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-09-05 18:24:17 +0800

    Remove redundant files and create portfolio route

--- investment-app/src/pages/portfolio/index.jsx
+++ investment-app/src/pages/portfolio/index.jsx
@@ -1,20 +1,11 @@
+import React from 'react';
 
-import Head from 'next/head'
-import {Button} from 'react-bootstrap'
-import { useRouter } from 'next/router'
-
-export default function Portfolio() {
-  const router = useRouter()
-
+const Portfolio = () => {
   return (
     <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Button onClick={() => router.push(router.pathname + '/hello')}>
-      Are y'all ready kids?
-      </Button>
+      
     </div>
-  )
+  );
 }
+
+export default Portfolio;
