e91ac09766cd5c5bcdfee1a589b1b20976256112
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-09-06 02:32:43 +0800

    Add depencies for quiz and add styling for watchlist

--- investment-app/src/components/portfolio/components/WatchListComponent.jsx
+++ investment-app/src/components/portfolio/components/WatchListComponent.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const WatchListComponent = (props) => {
+    const router = useRouter();
+
     return (
-        <tr>
+        <tr onClick={() => router.push("/portfolio/" + props.stock.key)} style={{ cursor: "pointer" }}>
             <td>{props.stock.key}</td>
             <td>{props.stock.price}</td>
             <td>Changes</td>
