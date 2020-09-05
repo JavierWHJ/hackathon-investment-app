faa405c44f53896a6653516c84012c8cddf70c68
jonathanlxh <jonathanlxh@users.noreply.github.com>
2020-09-05 15:30:08 +0800

    help i suck

--- investment-app/src/components/common/Loginmodal.jsx
+++ investment-app/src/components/common/Loginmodal.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {Modal, Button} from 'react-bootstrap'
+import {Modal, Button, Tabs} from 'react-bootstrap'
 
 const Loginmodal = () => {
     const [show, setShow] = useState(false);
@@ -15,29 +15,11 @@ const Loginmodal = () => {
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <ul class="nav nav-tabs" id="tabContent">
-                        <li class="active"><a href="#login" data-toggle="tab">Login</a></li>
-                        <li><a href="#signup" data-toggle="tab">Sign Up</a></li>
-                    </ul>
+                    Get Started
                 </Modal.Header>
-                    
                     <Modal.Body>   
-                        <div class='tab-content'>
-                            <div class="tab-pane active" id="login">
-                                <form>
-                                    <div class='form-group'>
-                                        <div class='email'><input placeholder='Email Address' name='email' type='email' id='email' class='s2 form-control'></input></div>
-                                    </div>
-                                    <div class='form-group'>
-                                        <div class='name'><input placeholder='Name' name='name' type='text' id='text' class='s2 form-control'></input></div>
-                                    </div>
-                                    <div class='text-center mb-3'>
-                                        <Button class='btn btn-primary btn-block'>Login</Button>
-                                    </div>
-                                </form>
-                            </div>
-                              
-                            <div class="tab-pane" id="signup">
+                        <div>
+                            <div>
                                 <form>
                                     <div class='form-group'>
                                         <div class='email'><input placeholder='Email Address' name='email' type='email' id='email' class='s2 form-control'></input></div>
@@ -47,6 +29,9 @@ const Loginmodal = () => {
                                     </div>
                                     <div class='text-center mb-3'>
                                         <Button class='btn btn-primary btn-block'>Sign Up</Button>
+                                    </div>
+                                    <div class='text-center mb-3'>
+                                    <Button class='btn btn- btn-block'>Log In</Button>
                                     </div>
                                 </form>
                             </div>
