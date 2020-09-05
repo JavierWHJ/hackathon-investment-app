c6a1c81197bc285e740523a5ee8c27830556a550
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-09-05 12:10:17 +0800

    Add header and remove redundant code

--- investment-app/src/components/common/Header.jsx
+++ investment-app/src/components/common/Header.jsx
@@ -4,7 +4,7 @@ import {Nav, Navbar} from 'react-bootstrap'
 const Header = () => {
     return (
         <>
-            <Navbar bg="light" variant="light">
+            <Navbar bg="light" variant="light" expand="lg">
                 <Navbar.Brand href="#">
                     <img
                         src="/pear.svg"
@@ -14,12 +14,15 @@ const Header = () => {
                     />{' '}
                     P.E.A.R
                 </Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Academy</Nav.Link>
-                    <Nav.Link href="#pricing">Financials</Nav.Link>
-                    <Nav.Link href="#pricing">Portfolio</Nav.Link>
-                </Nav>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Academy</Nav.Link>
+                        <Nav.Link href="#pricing">Financials</Nav.Link>
+                        <Nav.Link href="#pricing">Portfolio</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         </>
     );
