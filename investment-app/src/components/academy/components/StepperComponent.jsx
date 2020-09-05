526e9a10d180dad50f036905200ddfd4d1c5e199
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-09-05 14:25:40 +0800

    Add global primary colors and step content for steppers

--- investment-app/src/components/academy/components/StepperComponent.jsx
+++ investment-app/src/components/academy/components/StepperComponent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Stepper from "@material-ui/core/Stepper";
 import Step from "@material-ui/core/Step";
 import StepLabel from "@material-ui/core/StepLabel";
+import StepContent from "@material-ui/core/StepContent";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
@@ -14,6 +15,19 @@ const StepperComponent = (props) => {
                 {props.steps.map((label, index) => (
                     <Step key={label}>
                         <StepLabel>{label}</StepLabel>
+                        <StepContent>
+                            <ul className="pl-4">
+                                <li>
+                                    <a href="" onClick={(e) => e.preventDefault()}>hello</a>
+                                </li>
+                                <li>
+                                    <a href="" onClick={(e) => e.preventDefault()}>hello</a>
+                                </li>
+                                <li>
+                                    <a href="" onClick={(e) => e.preventDefault()}>hello</a>
+                                </li>
+                            </ul>
+                        </StepContent>
                     </Step>
                 ))}
             </Stepper>
