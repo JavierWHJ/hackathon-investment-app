ac07310fc689f80cca6549913621728b5c6cc370
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-09-06 17:58:54 +0800

    Add styling for percentage changes in watchlist

--- investment-app/src/components/portfolio/components/WatchListComponent.jsx
+++ investment-app/src/components/portfolio/components/WatchListComponent.jsx
@@ -4,6 +4,10 @@ import { useRouter } from "next/router";
 const WatchListComponent = (props) => {
     const router = useRouter();
 
+    const checkPercentageValue = (percentage) => {
+        return Math.sign(percentage) == 1;
+    }
+
     return (
         <tr
             key={props.stock}
@@ -12,7 +16,9 @@ const WatchListComponent = (props) => {
         >
             <td>{props.stock.symbol}</td>
             <td>{props.stock.price}</td>
-            <td>{props.stock.percentage}</td>
+            {checkPercentageValue(props.stock.percentage) ? 
+                <td><span className="text-success">{props.stock.percentage}</span></td> : 
+                <td><span className="text-danger">{props.stock.percentage}</span></td>}
         </tr>
     );
 };
