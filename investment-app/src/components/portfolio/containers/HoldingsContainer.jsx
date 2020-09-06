ae19b807374ddf9a2a1de6775f8c3908b10b3b5d
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-09-07 01:46:10 +0800

    Bug Fix and Add dependency

--- investment-app/src/components/portfolio/containers/HoldingsContainer.jsx
+++ investment-app/src/components/portfolio/containers/HoldingsContainer.jsx
@@ -4,25 +4,17 @@ import HoldingsComponent from "../components/HoldingsComponent";
 import TransactionComponent from "../components/TransactionComponent";
 
 const HoldingsContainer = (props) => {
-    console.log(props.userInfo);
-    console.log(props.holdingsPrices);
-
     var portfolioValue = props.userInfo.cash
-    
-    props.holdingsPrices.forEach(addPortfolioValue);
-
-    function addPortfolioValue(value, index, array) {
+    props.holdingsPrices.forEach((value) => {
         portfolioValue = portfolioValue + value.shares * value.currentPrice;
-    }
-
-    console.log(portfolioValue);
+    });
 
     return (
         <div className="mt-4 mx-5">
             <h2>{props.userInfo.name}</h2>
             <Tabs defaultActiveKey="account" id="uncontrolled-tab-example">
                 <Tab eventKey="account" title="Account">
-                    <h3 className="mt-4">${portfolioValue.toLocaleString('en')}</h3>
+                    <h3 className="mt-4">${parseInt(portfolioValue).toLocaleString('en')}</h3>
                     <p>Portfolio Value</p>
                     <Table className="mt-5" striped hover>
                         <thead>
